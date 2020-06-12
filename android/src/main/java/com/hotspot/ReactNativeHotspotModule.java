deff8e56b777b458ed5d2061ca14c640b84f08d1
Andrea Barletti <andrea.barletti@stride-it.com>
2020-06-12 11:58:13 +0200

    fixed missing import in module

--- android/src/main/java/com/hotspot/ReactNativeHotspotModule.java
+++ android/src/main/java/com/hotspot/ReactNativeHotspotModule.java
@@ -18,6 +18,13 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class ReactNativeHotspotModule extends ReactContextBaseJavaModule {
 
