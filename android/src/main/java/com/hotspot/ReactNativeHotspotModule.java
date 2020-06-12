0102c27c9b0de1980b754ac0286db827530af3d7
Andrea Barletti <andrea.barletti@stride-it.com>
2020-06-12 12:47:58 +0200

    implements LifecycleEventListener in ReactNativeHotspotModule

--- android/src/main/java/com/hotspot/ReactNativeHotspotModule.java
+++ android/src/main/java/com/hotspot/ReactNativeHotspotModule.java
@@ -26,7 +26,7 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-public class ReactNativeHotspotModule extends ReactContextBaseJavaModule {
+public class ReactNativeHotspotModule extends ReactContextBaseJavaModule implements LifecycleEventListener{
 
     private final ReactApplicationContext reactContext;
 
